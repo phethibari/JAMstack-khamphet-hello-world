e0505f866b4afb1e44717e809d98ee161fb8bc8d
khamphet_kph <khamphet046@gmail.com>
2018-08-28 13:45:49 +0700

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Khamphet Default Starter',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello everyone</h1>
+    <p>My name is </p>
+    <h3>Khamphet</h3>
+    <h2>LUONGPHAYBOUN.</h2>
+    <p>I am 22 years old.</p>
+    <p>I am software developer from Laos.</p>
+    <p>I have experience in coding Reactjs and Nodejs for 2-3 years.</p>
+    <p>I had been coding in Japan, Thailand and Laos.</p>
+  </div>
+)
+
+export default IndexPage
